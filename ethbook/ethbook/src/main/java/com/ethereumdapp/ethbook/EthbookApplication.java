d9b9426e00b63fa15c5a6c3c724eb38e990157a9
Masud karim <msmasud578@gmail.com>
2023-11-01 19:11:56 +0600

    initial commit

---
+++ ethbook/ethbook/src/main/java/com/ethereumdapp/ethbook/EthbookApplication.java
@@ -0,0 +1,13 @@
+package com.ethereumdapp.ethbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EthbookApplication.class, args);
+	}
+
+}
